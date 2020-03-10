2c130d0ddcc385721727eb9d06e9e51303614b84
JHolford <62012303+JHolford@users.noreply.github.com>
2020-03-10 10:30:31 +0000

    Initial Files

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
+<form action='login.php' method='post'>
+Event reference: <input type='text' name='eventref' id='eventref' /><br>
+Username: <input type='text' name='username' id='username' /><br>
+Password: <input type='password' name='password' id='password' /><br>
+<input type='submit' id='loginbutton' value='Login'/>
+</form>
+
+</body>
+</html>
