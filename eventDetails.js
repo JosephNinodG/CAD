f4f706f58070d9f80494df4a4dc863b43d8d8bd1
Jake Doran <info@jakedoran.co.uk>
2020-03-12 13:37:00 +0000

    Started working on file upload

--- eventDetails.js
+++ eventDetails.js
@@ -1,25 +1,106 @@
 $(document).ready(function() {
+    let completePercent = 12.5;
+
+     window.formValues = {
+        'shortDescription'  : $('body').find('#seminar-short-description').val(),
+        'longDescription'   : $('body').find('#seminar-long-description').val(),
+        'shortBiography'    : $('body').find('#seminar-short-biography').val(),
+        'longBiography'     : $('body').find('#seminar-long-biography').val(),
+    }
+
     $('body').on('click tap', '[data-target="save-details"]', function() {
         event.preventDefault();
-
-        console.log('clicked');
     });
 
     $('body').on('click tap', '[data-target="cancel-details"]', function() {
         event.preventDefault();
-
-        console.log('clicked');
     });
 
     $('body').on('click tap', '[data-target="upload-presentation"]', function() {
         event.preventDefault();
 
-        console.log('clicked');
+        $('body').find('#presentation-modal').modal('show');
     });
 
     $('body').on('click tap', '[data-target="upload-cover-image"]', function() {
         event.preventDefault();
+    });
 
-        console.log('clicked');
+    $('body #details-form').on('change paste', ':input', function() {
+        $.when(
+            updateProgressBar(completePercent)
+        ).done(function(percent) {
+            completePercent = percent;
+            animateProgressBar(completePercent);
+        });
+    });
+
+    $('body').on('click tap', '[data-trigger="file-upload"]', function(event) {
+        event.preventDefault();
+        
+        $.ajax({
+            url: 'fileUpload.php',
+            type: 'POST',
+            data: $('#presentation-form').serialize(),
+        });
     });
 });
+
+function updateProgressBar(completePercent) {
+    if (window.formValues.shortDescription == "" && $('body').find('#seminar-short-description').val().trim() != "") {
+        window.formValues.shortDescription = $('body').find('#seminar-short-description').val().trim();
+        completePercent += 12.5;
+    }
+
+    if (window.formValues.shortDescription != "" && $('body').find('#seminar-short-description').val().trim() == "") {
+        window.formValues.shortDescription = $('body').find('#seminar-short-description').val().trim();
+        completePercent -= 12.5;
+    }
+
+    if (window.formValues.longDescription == "" && $('body').find('#seminar-long-description').val().trim() != "") {
+        window.formValues.longDescription = $('body').find('#seminar-long-description').val().trim();
+        completePercent += 12.5;
+    }
+
+    if (window.formValues.longDescription != "" && $('body').find('#seminar-long-description').val().trim() == "") {
+        window.formValues.longDescription = $('body').find('#seminar-long-description').val().trim();
+        completePercent -= 12.5;
+    }
+
+    if (window.formValues.shortBiography == "" && $('body').find('#seminar-short-biography').val().trim() != "") {
+        window.formValues.shortBiography = $('body').find('#seminar-short-biography').val().trim();
+        completePercent += 12.5;
+    }
+
+    if (window.formValues.shortBiography != "" && $('body').find('#seminar-short-biography').val().trim() == "") {
+        window.formValues.shortBiography = $('body').find('#seminar-short-biography').val().trim();
+        completePercent -= 12.5;
+    }
+
+    if (window.formValues.longBiography == "" && $('body').find('#seminar-long-biography').val().trim() != "") {
+        window.formValues.longBiography = $('body').find('#seminar-long-biography').val().trim();
+        completePercent += 12.5;
+    }
+
+    if (window.formValues.longBiography != "" && $('body').find('#seminar-long-biography').val().trim() == "") {
+        window.formValues.longBiography = $('body').find('#seminar-long-biography').val().trim();
+        completePercent -= 12.5;
+    }
+
+    // Add in Image + Presentations
+
+    return completePercent;
+}
+
+function animateProgressBar(completePercent) {
+    let percent = completePercent;
+
+    percent = percent + '%';
+
+    $(".progress-bar").animate({
+        width: percent
+    }, 1000);
+
+    $(".progress-bar").attr('aria-valuenow', percent);
+    $(".progress-bar").html(percent);
+}
--- eventDetails.php
+++ eventDetails.php
@@ -3,15 +3,16 @@
     <head>
         <meta charset="utf-8">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="master.css">
         <title>Event Details</title>
     </head>
     <body>
         <div class="container">
-            <form class="needs-validation" id="details-form" novalidate>
+            <form id="details-form" >
                 <div class="row">
                     <div class="col">
                         <div class="progress">
-                            <div class="progress-bar" role="progressbar" style="width: 10%;" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">10%</div>
+                            <div class="progress-bar" role="progressbar" style="width: 12.5%;" aria-valuenow="12.5" aria-valuemin="0" aria-valuemax="100">12.5%</div>
                         </div>
                     </div>
                 </div>
@@ -29,8 +30,14 @@
                     <div class="col">
                         <div class="form-group">
                             <label for="seminar-title">Title</label>
-                            <input type="email" class="form-control" id="seminar-title" aria-describedby="title-help" readonly>
+                            <input type="text" name="seminar-title" class="form-control" id="seminar-title" aria-describedby="title-help" readonly>
                             <small id="title-help" class="form-text text-muted">This has already been set for you.</small>
+                        </div>
+                        <div class="form-group">
+                            <label for="seminar-title">Presentation Type</label>
+                            <select class="form-control" id="seminar-type">
+                                <option value="">Select</option>
+                            </select>
                         </div>
                         <div class="form-group">
                             <label for="seminar-short-description">Short Description</label>
@@ -63,9 +70,48 @@
                 </div>
             </form>
         </div>
+        <!-- Modals -->
+        <div id="presentation-modal" class="modal" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Upload Presentation</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form id="presentation-form" enctype="multipart/form-data">
+                            <div class="form-group">
+                                <label for="presentation-title">Document Title</label>
+                                <input type="text" class="form-control" id="presentation-title" name="presentation-title">
+                            </div>
+                            <div class="form-group">
+                                <label for="presentation-name">File Name</label>
+                                <input type="text" class="form-control" id="presentation-name" name="presentation-name">
+                            </div>
+                            <div class="form-group">
+                                <label for="presentation-purpose">File Purpose</label>
+                                <input type="text" class="form-control" id="presentation-purpose" name="presentation-purpose">
+                            </div>
+                            <div class="form-group">
+                                <input id="presentation-file" name="presentation-file[]" type="file" />
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-dark" data-trigger="file-upload">UPLOAD</button>
+                        <button type="button" class="btn btn-warning" data-dismiss="modal">CANCEL</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
     <footer>
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
         <script src="eventDetails.js" type="text/javascript"></script>
