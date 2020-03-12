7c34b2c2f06a8bcaaad2d0c3c03be002554e7965
JHolford <62012303+JHolford@users.noreply.github.com>
2020-03-12 10:43:23 +0000

    Adjusted how the time is presented

--- seminars.php
+++ seminars.php
@@ -1,5 +1,3 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
 <?php
 session_start();
 $apikey = $_SESSION['apikey'];
@@ -10,44 +8,12 @@ if(!isset($apikey) || $apikey==''){
 $apiurl = "https://reg.bookmein2.com/api/checkinapi.php";
 
 $data = array('apikey'=>$apikey, 'action' => "getlocationlist");
-$conferenceNameData = array('apikey'=>$apikey, 'action' => "getconferencename");
+
 $ch = curl_init();
 
 $params = http_build_query($data);
-$paramsConfName = http_build_query($conferenceNameData);
 $getUrl = $apiurl."?".$params;
-$getConferenceNameUrl = $apiurl."?".$paramsConfName;
 
-
-curl_setopt($ch, CURLOPT_URL, $getConferenceNameUrl);
-
-//return the transfer as a string
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-// $output contains the output string
-$confNameOutput = curl_exec($ch);
-
-// close curl resource to free up system resources
-curl_close($ch);
-
-$confNameResults = json_decode($confNameOutput);
-
-$confNameArray = array();
-$confNameSuccess = true;
-
-if($confNameResults->success){
-		$confName=$confNameResults->data;
-
-
-}else{
-	$confNameSuccess = false;
-	$error = $confNameResults->error;
-}
-
-
-
-
-$ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $getUrl);
 
 //return the transfer as a string
@@ -57,7 +23,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $output = curl_exec($ch);
 
 // close curl resource to free up system resources
-curl_close($ch);
+curl_close($ch);  
 
 $results = json_decode($output);
 
@@ -77,64 +43,39 @@ if($results->success){
 <!DOCTYPE html>
 <html>
 <head>
-<style>
-	body {
-		background-color: black;
-	}
-	.seminarBox {
-		background-color: white;
-	}
-	.percentageComp {
-		text-align:right;
-		color:red;
-	}
-	hr {
-		border: 3px solid black;
-		border-radius:2px;
-		margin-right:5px;
-		margin-left:5px;
-	}
 
-</style>
 </head>
 <body>
-<div class="container-fluid">
+
+<table id='seminars' border='1'>
+<thead><tr><th>ID</th><th>Type</th><th>Name</th></thead>
+<tbody id='seminarbody'>
 <?php
 if($success){
 	$seminarCount = 0;
 	foreach($seminars as $seminar){
 		if ($seminar->event_type=="Seminar") {
-		echo "<div class='seminarBox'>";
-		echo "<div class='row'>";
-		echo "<div class='col-4'>$confName</div>";
-		echo "<div class='col-sm'>Start: {$seminar->start_time}</div>";
-		echo "<div class='col-sm'>End: {$seminar->end_time}</div>";
-		echo "<div class='col-sm percentageComp' >0% complete! </div>";
-		echo "</div>";
-		echo "<hr/>";
-		echo "<div class='row'>";
-		// echo "<div class='col-sm'>{$seminar->id}</div>";
-		// echo "<div class='col-sm'>{$seminar->event_type}</div>";
-		echo "<div class='col'><h2>{$seminar->name}</h2></div>";
-		echo "</div>";
-		echo "</div>";
-
-		echo "<br/><br/>";
-
+		echo "<tr>";
+		echo "<td>{$seminar->id}</td>";
+		echo "<td>{$seminar->event_type}</td>";
+		echo "<td>{$seminar->name}</td>";
+		echo "</tr>";
 		$seminarCount++;
 		}
 	}
-
+	
 }else{
-	echo "<div class='row'>Unable to get results: $error</div>";
-}
-
-echo ("<div class='row'> $seminarCount locations found</div>");
+	echo "<tr><td colspan='3'>Unable to get results: $error</td></tr>";
+}	
+?>
+</tbody>
+</table>
+<?php 
+echo ("$seminarCount locations found");
 ?>
 <div id='feedback'>
 
-</div><!-- This closes the feedback div -->
-</div><!-- This closes the bootsrap container -->
+</div>
 
 </body>
 </html>
