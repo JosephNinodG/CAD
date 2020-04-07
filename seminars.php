fb058c6e8673c2c47ddfc0fc28a4ff8d255a5b85
Jake Doran <jakedoran@Jakes-MacBook-Pro.local>
2020-04-07 09:51:01 +0100

    Started tying in Joe+Joe work with mine

--- seminars.php
+++ seminars.php
@@ -115,7 +115,7 @@ if($success){
 	foreach($seminars as $seminar){
 		if ($seminar->event_type=="Seminar") {
 
-		echo "<div class='seminarBox' onclick=window.open('sidebar.php?id=$seminar->id')>";
+		echo "<div class='seminarBox' data-trigger='view-event' data-id='$seminar->id'>";
 		echo "<div class='row'>";
 		echo "<div class='col-5'>$confName</div>";
 		$dateOf=date('M j', strtotime($seminar->start_time));
@@ -153,4 +153,10 @@ echo ("<div class='row'> $seminarCount locations found</div>");
 </div><!-- This closes the bootsrap container -->
 
 </body>
+<footer>
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	<script src="seminars.js" type="text/javascript"></script>
+</footer>
 </html>
