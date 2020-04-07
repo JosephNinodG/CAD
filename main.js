a047aa459bfee22e9a4930fbd8b3a174363967ae
Jake Doran <jakedoran@Jakes-MacBook-Pro.local>
2020-04-07 12:38:18 +0100

    Fixed a couple bugs with back/forward buttons

--- main.js
+++ main.js
@@ -1,18 +1,41 @@
 $(document).ready(function() {
+	checkReload();
+
+	window.onpopstate = function(event) {
+		if (document.location.href.includes('editEventDetails.php')) {
+			loadView('editEventDetails.php');
+			return;
+		}
+
+		if (document.location.href.includes('sidebar.php')) {
+			loadView('seminars.php');
+			return;
+		}
+	}
+
 	loadView('seminars.php');
 
+	// Causes issues with back button
 	$('body').on('click tap', '[data-trigger="view-event"]', function(event) {
 		event.preventDefault();
 
 		let id = $(this).attr('data-id');
 
-		let url = window.location.href;
+		let url = window.location.hostname;
 
-		history.pushState({}, null, url+`?id=${id}`);
+		history.pushState({}, null, `/CAD/editEventDetails.php?id=${id}`);
 
 		loadView('editEventDetails.php');
 	});
 });
+
+function checkReload() {
+	// This isn't quite working yet
+	if (document.location.href.includes('sidebar.php?id=')) {
+		loadView('editEventDetails.php');
+		return;
+	}
+}
 
 function loadView(url) {
 	$.when(
