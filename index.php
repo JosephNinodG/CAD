41a600075f110b7bcefa7d6bcf521b74489e320d
JosephNinodG <61060613+JosephNinodG@users.noreply.github.com>
2020-04-28 14:20:39 +0100

    Login page (Final)
    Code for page, stylesheet and login.php all refined and commented. Files renamed to integrate with project easier.

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> <!-- initialise page and import bootstrap stylesheet -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Login Page</title>
+    <link rel="stylesheet" type="text/css" href="indexSS.css"> <!-- import specific page stylesheet -->
+</head>
+<body>
+    <header> <!-- Place BookMeIn2 logo in header with embedded link to redirect to main website -->
+        <div id="logocontainer">
+            <a href="https://www.bookmein2.com">
+                <img src="logo.png" alt="BookMeIn2" id="logo">
+            </a>
+        </div>
+    </header>
+    <div class="banner"></div> <!-- div block for page structure -->
+    <div class="content"> <!-- Main content goes here -->
+        <div class="container"> <!-- bootstrap container used (not container-fluid as that is full page width) -->
+            <div class="row"> 
+                <div class="col-xs-0 col-sm-0 col-md-2"> <!-- col used to help center login form (cols are removed at certain page size) -->
+                    <div class="blackspace"></div> <!-- div block for page structure -->
+                </div>
+                <div class="col">
+                    <?php if(isset($_GET['badpass'])){echo "<div class='alert alert-danger' role='alert'>Incorrect login information provided</div>";}?> <!-- login error message displayed if login.php returns badpass=1 after incorrect login info provided -->
+                    <form action='login.php' method='post' id="loginform"> <!-- login form created that is linked to the login.php file -->
+                            <label id="loginlabel">Login</label>
+                        <div class="form-group"> <!-- (bootstrap class used) -->
+                            <label for="eventref" class="labelstyle">Event Reference</label>
+                            <input type="text" name="eventref" class="form-control" id="eventref" placeholder="Event Reference"> <!-- Event ref input box (bootstrap class used) -->
+                        </div>
+                        <div class="form-group"> <!-- (bootstrap class used) -->
+                            <label for="username" class="labelstyle">Username</label>
+                            <input type="text" name="username" class="form-control" id="username" placeholder="Username"> <!-- username input box (bootstrap class used) -->
+                            <!-- change to type="email" -->
+                        </div>
+                        <div class="form-group"> <!-- (bootstrap class used) -->
+                            <label for="password" class="labelstyle">Password</label>
+                            <input type="password" name="password" class="form-control" id="password" placeholder="Password"> <!-- password input box (bootstrap class used) -->
+                        </div>
+                        <button type='submit' class="btn btn-primary" id='formbutton'>Login</button> <!-- login button (bootstrap class used) -->
+                    </form>
+                </div>
+                <div class="col-xs-0 col-sm-0 col-md-2">
+                    <div class="blackspace"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="banner"></div>
+    <footer> <!-- textlink to the BookMeIn2 Terms and Conditions and Privacy Statement -->
+        <div class="TandC-PrivacyStatment">
+            <a href="https://www.bookmein2.com/App_Terms_BMi2_v2_1_5-v002.pdf">Terms and Conditions and Privacy Statement</a>
+        </div>
+    </footer>
+    <!-- import scripts for bootstrap -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
-session_start();
-$apiurl = "https://reg.bookmein2.com/api/checkinapi.php";
-$data = array(
+session_start(); //start new session
+$apiurl = "https://reg.bookmein2.com/api/checkinapi.php"; //stores api url
+$data = array( //stores data from login page form in array 
 					'username' => $_POST['username'],
 					'password' => $_POST['password'],
 					'eventref' => $_POST['eventref'],
@@ -25,16 +25,18 @@ curl_close($ch);
 
 $results = json_decode($output);
 
-if($results->success){
-	if($results->data->loggedin){
+if($results->success){ //if can access api then success
+	if($results->data->loggedin){ // if login info correct then log in
 		$_SESSION['apikey'] = $results->data->apikey;
-		header("Location: Dashboard.php");
+		header("Location: seminars.php"); //redirect to seminars.php, accessing the main user page
 		exit;
 	}else{
-		header("Location: BMI2Login.php?badpass=1");
+		header("Location: index.php?badpass=1"); // if login info incorrect redirect back to login page with badpass=1 to allow error message to display
 	}
 }else{
-	echo "Unable to login.  Error: ".$results->error;
+	echo "Unable to login.  Error: ".$results->error; // if cannot access api, display error message and link to take back to login page
+	echo "<br/>";
+	echo "<a href='index.php'>Return to Login Page</a>";
 }
 
 ?>
